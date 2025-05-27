88d01109ff530553c98d59814ce3d25d7f74f554
Alexey Chapski <alexey.chapski@gmail.com>
2025-05-27 18:40:23 +0400

    feat: add new layouts components

--- src/screens/ForgotPasswordScreen.tsx
+++ src/screens/ForgotPasswordScreen.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Keyboard } from 'react-native';
 
 import { ScreenProps } from '@src/navigation/types';
 import { useAppTheme } from '@src/theme/theme';
 import { useLocalization } from '@src/translations/i18n';
 import { Box, Button, Input } from '@src/ui';
+import { FakeView } from '@src/ui/Layouts/FakeView';
+import { Gap } from '@src/ui/Layouts/Gap';
 import { validator } from '@src/utils/validations';
 
 
 const ForgotPasswordScreen = ({ navigation }: ScreenProps<'forgot-password'>) => {
   const { insets } = useAppTheme();
-
   const { t } = useLocalization()
 
   const { control, handleSubmit } = useForm({
@@ -24,53 +25,43 @@ const ForgotPasswordScreen = ({ navigation }: ScreenProps<'forgot-password'>) =>
     navigation.navigate('tabs')
   }
   return (
-    <KeyboardAwareScrollView
-      contentContainerStyle={{ flex: 1 }}
-      keyboardShouldPersistTaps="handled"
-    >
-      <Box
-        pr={16}
-        pl={16}
-        pt={insets.top + 82}
-        gap={24}
-        pb={insets.bottom + 35}
-        alignItems='center'
-        flex={1} >
-        <Box flex={1} w='full' gap={24}>
-          <Box gap={16} w='full'>
-            <Controller
-              control={control}
-              name='email'
-              rules={{ required: true, validate: validator.email }}
-              render={({ field: { value, onBlur, onChange }, fieldState }) => (
-                <Input
-                  required
-                  value={value}
-                  onChangeText={onChange}
-                  onBlur={onBlur}
-                  placeholder='E-mail'
-                  error={fieldState.invalid}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  textContentType="username"
-                  inputMode="email"
-                  autoComplete="username"
-                  returnKeyType="next"
-                  importantForAutofill="yes"
-                  autoFocus
-                />
-              )}
-            />
-          </Box>
-          <Button
-            children={t('confirm')}
-            onPress={handleSubmit(handleSendOtpToResetPassword)}
-          />
-        </Box>
 
+    <Box accessible={false} onPress={Keyboard.dismiss} effect="none" flex={1} pr={20} pl={20}>
+      <Box flex={1}>
+        <Controller
+          control={control}
+          name='email'
+          rules={{ required: true, validate: validator.email }}
+          render={({ field: { value, onBlur, onChange }, fieldState }) => (
+            <Input
+              required
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder='E-mail'
+              error={fieldState.invalid}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              textContentType="username"
+              inputMode="email"
+              autoComplete="username"
+              returnKeyType='done'
+              importantForAutofill="yes"
+              autoFocus
+              prompting='Укажите E-mail на который создавали аккаунт и мы вышлем на него ссылку для восстановления пароля'
+            />
+          )}
+        />
       </Box>
-    </KeyboardAwareScrollView>
+
+      <Button
+        children={t('confirm')}
+        onPress={handleSubmit(handleSendOtpToResetPassword)}
+      />
+      <FakeView additionalOffset={-insets.bottom / 2} />
+      <Gap y={insets.bottom || 16} />
+    </Box>
   );
 };
 
