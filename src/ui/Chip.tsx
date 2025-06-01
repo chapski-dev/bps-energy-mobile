86ee65074bb83bb3ee6b1d2d9fde88e90dc4df8c
Alexey Chapski <alexey.chapski@gmail.com>
2025-06-01 19:06:48 +0300

    feat: improve Chip

--- src/ui/Chip.tsx
+++ src/ui/Chip.tsx
@@ -1,15 +1,16 @@
 import React, { PropsWithChildren } from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
 
 import { useAppTheme } from '@src/theme/theme';
 
 import { Box } from './Box';
 import { Text } from './Text';
 
-export const Chip = ({ children }: PropsWithChildren) => {
+export const Chip = ({ children, style }: PropsWithChildren & { style?: StyleProp<ViewStyle> }) => {
   const { colors } = useAppTheme();
 
   return (
-    <Box backgroundColor={colors.grey_50} px={10} borderRadius={5}>
+    <Box backgroundColor={colors.grey_50} px={10} borderRadius={5} style={style}>
       <Text children={children} colorName='grey_700' />
     </Box>
   );
