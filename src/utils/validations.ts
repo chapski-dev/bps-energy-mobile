4564dd6a3bf782bd8ef3c8c1c47f1f51ff12b287
Alexey Chapski <alexey.chapski@gmail.com>
2025-06-03 15:54:28 +0300

    feat: improve validator

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -8,6 +8,9 @@ export const validator = {
     // const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@?\-*=+!./#$&])[a-zA-Z0-9@?\-*=+!./#$&]{6,}$/;
     // password_does_not_meet_requirements
     // return regex.test(val);
+    if(val.length < 6) {
+      return 'Длина пароля должна быть минимум 6 символов'
+    };
     return true;
   }
 }
