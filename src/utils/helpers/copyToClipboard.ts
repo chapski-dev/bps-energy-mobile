b3ba255705b546d00b835353c40a55ee56791b99
Alexey Chapski <alexey.chapski@gmail.com>
2025-06-24 18:02:18 +0300

    chore: replace some utils to 'helpers' folder

--- src/utils/helpers/copyToClipboard.ts
+++ src/utils/helpers/copyToClipboard.ts
@@ -2,7 +2,7 @@ import { HapticFeedbackTypes } from 'react-native-haptic-feedback';
 import { toast } from '@backpackapp-io/react-native-toast'
 import Clipboard from '@react-native-clipboard/clipboard'
 
-import { vibrate } from './vibrate';
+import { vibrate } from '../vibrate';
 
 export const copyToClipboard = (textToCopy: string, message: string) => {
   Clipboard.setString(textToCopy)
