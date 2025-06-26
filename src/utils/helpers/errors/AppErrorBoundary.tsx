39b5cdcbcf2f532f817f770f5556cd53a290b4d2
Alexey Chapski <alexey.chapski@gmail.com>
2025-06-26 16:45:59 +0300

    feat: improve AppErrorBoundary layout

--- src/utils/helpers/errors/AppErrorBoundary.tsx
+++ src/utils/helpers/errors/AppErrorBoundary.tsx
@@ -1,26 +1,50 @@
 import React, { PropsWithChildren } from 'react'
 import { useTranslation } from 'react-i18next'
 import ErrorBoundary from 'react-native-error-boundary'
+import TryAgainIcon from '@assets/svg/arrow-counter-clockwise.svg';
+import EnvelopIcon from '@assets/svg/envelope.svg';
 import WarningCircleIcon from '@assets/svg/warning-circle.svg';
 
 import { useAppTheme } from '@src/theme/theme';
 import { Box, Button, Text } from '@src/ui'
 
+import { CrashHandler } from './CrashHandler';
+
 const CustomFallback = (props: { error: Error, resetError: () => void }) => {
-  const { t } = useTranslation('errors') 
+  const { t } = useTranslation('errors')
   const { colors } = useAppTheme();
 
   return (
-  <Box flex={1} justifyContent='center' alignItems='center' px={16} gap={50}> 
-    <WarningCircleIcon color={colors.main} width={130} height={130} />
-    <Box gap={15}>
-    <Text variant='h2' center children={t('critical.error-boundary-title')} />
-    <Text variant='p1' center children={t('critical.error-boundary-message')} />
-    <Text>{props.error.toString()}</Text>
+    <Box flex={1} justifyContent='center' alignItems='center' px={16} gap={24}>
+      <WarningCircleIcon color={colors.main} width={72} height={72} />
+      <Box gap={8}>
+        <Text variant='h5' center children={t('critical.error-boundary-title')} />
+        <Text
+          variant='p3'
+          colorName="grey_600"
+          center
+          px={20}
+          children={t('critical.error-boundary-message')}
+        />
+      </Box>
+      <Box gap={12} alignItems='center'>
+        <Button
+          onPress={props.resetError}
+          children={t('critical.try-again')}
+          width="auto"
+          icon={<TryAgainIcon />}
+        />
+        <Button
+          type="outline"
+          width='auto'
+          icon={<EnvelopIcon color={colors.grey_800} />}
+          onPress={() => CrashHandler.handleFatalError(props.error, 'JavaScript')}
+          children={t('critical.send-report')}
+        />
+      </Box>
     </Box>
-    <Button onPress={props.resetError} children={t('critical.try-again')} />
-  </Box>
-)}
+  )
+}
 
 export const AppErrorBoundary = ({ children }: PropsWithChildren) => (
   <ErrorBoundary FallbackComponent={CustomFallback}
